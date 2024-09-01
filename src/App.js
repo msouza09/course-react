f9f532a9a773d8f0591843feea4493280bf54769
Matheus <szmatheussouza@gmail.com>
2024-09-01 16:48:35 -0300

    biscoito-sorte

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from "react"
+import React, {Component} from "react";
+import './estilo.css'
 
-export default function App(){
-  return(
-    <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>@msouza09</h2>
-    </div>
-  )
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+    
+    this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'];
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = ' " ' + this.frases[numeroAleatorio] + ' " ';
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container">
+          <img src={require('./assets/biscoito.png')} className="img" />
+          <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+          <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
 }
+
+class Botao extends Component {
+  render(){
+    return(
+      <div>
+        <button type="button" class="btn btn-primary" onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    );
+  }
+}
+
+export default App;
