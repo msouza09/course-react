7e890be0b0742a748d7eb72481a3a9353bf24610
Matheus <szmatheussouza@gmail.com>
2024-09-01 15:54:51 -0300

    ajuste-código

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div>
+      <h1>Bem vindo ao sistema</h1>
+      <h2>@msouza09</h2>
     </div>
-  );
+  )
 }
-
-export default App;
