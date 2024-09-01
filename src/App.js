9468e9a3237ec0611fc17a9511c77a23a58bb798
Matheus <szmatheussouza@gmail.com>
2024-09-01 15:57:13 -0300

    state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from "react"
+import React, {Component} from "react";
 
-export default function App(){
-  return(
-    <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>@msouza09</h2>
-    </div>
-  )
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Matheus',
+      contador: 0
+    };
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar(){
+    let state = this.state;
+    state.contador += 1;
+    state.nome = 'Lucas';
+    this.setState(state)
+  }
+
+  diminuir(){
+    let state = this.state;
+    if (state.contador === 0) {
+      alert('Opa chegou a zero!');
+      return;
+    }
+
+    state.contador -= 1;
+    this.setState(state);
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Contador</h1>
+          {this.state.nome}
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+            {this.state.contador} 
+          <button onClick={this.aumentar} >+</button></h3>
+      </div>
+    );
+  }
 }
+
+export default App;
